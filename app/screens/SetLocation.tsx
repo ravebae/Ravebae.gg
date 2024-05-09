df7a79d8ec7b29cca6279f9282287f9d2188768e
jigmey <jigmeydorjee1@protonmail.com>
2024-05-09 15:24:11 +1000

    stuff

--- app/screens/SetLocation.tsx
+++ app/screens/SetLocation.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { View, Text, Button, Image } from 'react-native';
+import { Button, Picker, TouchableHighlight } from 'react-native';
 import { Controller, useForm } from 'react-hook-form';
-import { Picker } from '@react-native-picker/picker';
-import { FIREBASE_AUTH } from '../../firebaseConfig';
 import S from './styles'; 
 
 export const SetLocation = ({ navigation }) => {
@@ -12,34 +10,34 @@ export const SetLocation = ({ navigation }) => {
 
     const onSubmit = data => {
         console.log('Location selected:', data.location);
-    
+        
     };
 
     return (
-        <View style={S.container}>
-            <View style={S.header}>
-                <Button title="<" onPress={() => navigation.goBack()} /> 
-                <Text style={S.title}>Set Your Location</Text>
-            </View>
-            <Text style={S.subtitle}>WHERE ALL THE RAVEBAES AT?</Text>
-            <Controller
-                name="location"
-                control={control}
-                render={({ field: { onChange, value } }) => (
-                    <Picker
-                        selectedValue={value}
-                        onValueChange={onChange}
-                        style={S.picker}
-                    >
-                        <Picker.Item label="Sydney" value="Sydney" />
-                        <Picker.Item label="Melbourne" value="Melbourne" />
-                        <Picker.Item label="Brisbane" value="Brisbane" />
-                        
-                    </Picker>
-                )}
-            />
-            <Button title="Continue" onPress={handleSubmit(onSubmit)} style={S.continueButton} />
-        </View>
+        <S.ProfileContainer>
+            <S.Title>Set Your Location</S.Title>
+            <S.FormWrapper>
+                <Text style={{ color: '#fff', fontSize: 16, marginBottom: 20 }}>WHERE ALL THE RAVEBAES AT?</Text>
+                <Controller
+                    name="location"
+                    control={control}
+                    render={({ field: { onChange, value } }) => (
+                        <Picker
+                            selectedValue={value}
+                            onValueChange={onChange}
+                            style={{ backgroundColor: '#fff', color: '#000', marginBottom: 20 }}
+                        >
+                            <Picker.Item label="Sydney" value="Sydney" />
+                            <Picker.Item label="Melbourne" value="Melbourne" />
+                            <Picker.Item label="Brisbane" value="Brisbane" />
+                        </Picker>
+                    )}
+                />
+                <TouchableHighlight onPress={handleSubmit(onSubmit)} style={S.ContinueBtn}>
+                    <S.Continue>Continue</S.Continue>
+                </TouchableHighlight>
+            </S.FormWrapper>
+        </S.ProfileContainer>
     );
 };
 

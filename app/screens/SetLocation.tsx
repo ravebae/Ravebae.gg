e195a6d9a42d2ab0e1b46f0fb57ae54db251a047
jigmey <jigmeydorjee1@protonmail.com>
2024-05-09 21:57:49 +1000

    stuff

--- app/screens/SetLocation.tsx
+++ app/screens/SetLocation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, TouchableHighlight, Text } from 'react-native';
+import { Button, TouchableHighlight, Text, Image } from 'react-native';
 import { Controller, useForm } from 'react-hook-form';
 import S from './styles';
 import Dropdown from '@components/Dropdown';
@@ -11,56 +11,51 @@ const locationData = [
 ];
 
 export const SetLocation = ({ navigation }) => {
-  const { control, handleSubmit } = useForm({
-    defaultValues: { location: '' },
-  });
-
-  const onSubmit = (data) => {
-    console.log('Location selected:', data.location);
+    const { control, handleSubmit } = useForm({
+      defaultValues: { location: '' },
+    });
+  
+    const onSubmit = (data) => {
+      console.log('Location selected:', data.location);
+    };
+  
+    return (
+      <S.ProfileContainer>
+        <S.Title style={{ fontFamily: 'JockeyOne_400Regular' }}>Set Your Location</S.Title>
+        <S.FormWrapper>
+          <Text style={{ color: '#fff', fontSize: 16, marginBottom: 20, fontFamily: 'JockeyOne_400Regular' }}>
+            WHERE ALL THE RAVEBAES AT?
+          </Text>
+          <Controller
+            name='location'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Dropdown
+                data={locationData}
+                selectedValue={value}
+                onValueChange={onChange}
+                style={{
+                  backgroundColor: '#fff',
+                  color: '#000',
+                  marginBottom: 20,
+                  fontFamily: 'JockeyOne_400Regular'  
+                }}
+              />
+            )}
+          />
+          <Image
+            source={require('./assets/location_person.png')} // Adjust path as necessary
+            style={{ width: '100%', height: 200, marginBottom: 20 }} // Adjust styling as necessary
+          />
+          <TouchableHighlight onPress={handleSubmit(onSubmit)}>
+            <S.ContinueBtn>
+              <S.Continue style={{ fontFamily: 'JockeyOne_400Regular' }}>Continue</S.Continue>
+            </S.ContinueBtn>
+          </TouchableHighlight>
+        </S.FormWrapper>
+      </S.ProfileContainer>
+    );
   };
-
-  return (
-    <S.ProfileContainer>
-      <S.Title style={{ fontFamily: 'JockeyOne_400Regular' }}>Set Your Location</S.Title>
-      <S.FormWrapper>
-        <Text style={{ color: '#fff', fontSize: 16, marginBottom: 20, fontFamily: 'JockeyOne_400Regular' }}>
-          WHERE ALL THE RAVEBAES AT?
-        </Text>
-        <Controller
-          name='location'
-          control={control}
-          render={({ field: { onChange, value } }) => (
-            <Dropdown
-              data={locationData}
-              selectedValue={value}
-              onValueChange={onChange}
-              style={{
-                backgroundColor: '#fff',
-                color: '#000',
-                marginBottom: 20,
-                fontFamily: 'JockeyOne_400Regular'  
-              }}
-            />
-          )}
-        />
-         <TouchableHighlight onPress={handleSubmit(onSubmit)}>
-          <S.ContinueBtn>
-            <S.Continue
-              style={{ fontFamily: 'JockeyOne_400Regular' }}
-              onPress={() => {
-                formStore.update((s) => {
-                  s.page += 1;
-                });
-                console.log(formStore);
-              }}
-            >
-              Continue
-            </S.Continue>
-          </S.ContinueBtn>
-        </TouchableHighlight>
-      </S.FormWrapper>
-    </S.ProfileContainer>
-  );
-};
+  
 
 export default SetLocation;

e6f96414f0d5107741bb0bb4c5ea2ad042e436cb
Christian Lam <christian.lam0103@gmail.com>
2024-05-10 11:00:13 +1000

    feat: Update design

--- app/screens/ProfileDetails/Email.tsx
+++ app/screens/ProfileDetails/Email.tsx
@@ -6,49 +6,49 @@ import { Controller, useForm } from 'react-hook-form';
 import Inputbox from '@components/Inputbox';
 import Dropdown from '@components/Dropdown';
 import { formStore } from 'store';
-import { MotiView } from 'moti';
-import { FIREBASE_AUTH } from 'firebaseConfig';
 
 const Email = () => {
   const { register, control, handleSubmit } = useForm({
     defaultValues: formStore.useState((s) => s),
   });
 
+  const handleContinue = () => {
+    formStore.update((s) => {
+      s.page += 1;
+    });
+  };
+
   return (
-    <S.ViewContainer>
-      <S.ScrollContainer
-        nestedScrollEnabled={true}
-        contentContainerStyle={{ flexGrow: 1 }}
-      >
-        <S.EmailContainer>
-          <S.FormWrapper>
-            <S.Title style={{ fontFamily: 'JockeyOne_400Regular' }}>
-              {'My Email'}
-            </S.Title>
-            <MotiView
-              from={{ opacity: 0, left: 100 }}
-              animate={{ opacity: 1, left: 0 }}
-              transition={{ type: 'timing' }}
-            >
-              <Controller
-                name='email'
-                control={control}
-                rules={{}}
-                render={({ field: { onChange, onBlur, value } }) => (
-                  <Inputbox
-                    placeholder='Email'
-                    onChange={onChange}
-                    onBlur={onBlur}
-                    value={value}
-                  />
-                )}
-              />
-              <Text>Hello</Text>
-            </MotiView>
-          </S.FormWrapper>
-        </S.EmailContainer>
-      </S.ScrollContainer>
-    </S.ViewContainer>
+    <S.FormWrapper>
+      <Controller
+        name='email'
+        control={control}
+        rules={{}}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Inputbox
+            placeholder='Email'
+            onChange={onChange}
+            onBlur={onBlur}
+            value={value}
+          />
+        )}
+      />
+      <TouchableHighlight onPress={handleContinue}>
+        <S.ContinueBtn>
+          <S.Continue
+            style={{ fontFamily: 'JockeyOne_400Regular' }}
+            onPress={() => {
+              formStore.update((s) => {
+                s.page += 1;
+              });
+              console.log(formStore);
+            }}
+          >
+            Continue
+          </S.Continue>
+        </S.ContinueBtn>
+      </TouchableHighlight>
+    </S.FormWrapper>
   );
 };
 
--- app/screens/ProfileDetails/styles.ts
+++ app/screens/ProfileDetails/styles.ts
@@ -28,21 +28,22 @@ const ScrollContainer = styled(ScrollView)`
   flex: 1;
 `;
 
-const ProfileContainer = styled(KeyboardAvoidingView)`
-  padding: 60px 30px;
+const ProfileContainer = styled(View)`
+  padding: 80px 30px;
   height: 100%;
   overflow-y: scroll;
+  flex-grow: 0;
 `;
 
 const PreviousBtn = styled(TouchableOpacity)`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 48px;
-  height: 48px;
-  padding: 12px;
+  width: 45px;
+  height: 45px;
+  padding: 5px;
   border: 1px solid white;
-  border-radius: 12px;
+  border-radius: 10px;
   background-color: #031013;
 `;
 
@@ -81,8 +82,6 @@ const ContinueBtn = styled(View)`
 
 const ComponentWrapper = styled(View)`
   margin: 10px 0px;
-  height: 100%;
-  flex: 1;
 `;
 
 const CategoryContainer = styled(View)`

22f846014b8a6a73cd8f9e58da70b0d907cc3fdf
Christian Lam <christian.lam0103@gmail.com>
2024-06-20 01:18:04 +1000

    feat: Update intro

--- app/components/Inputbox/Inputbox.tsx
+++ app/components/Inputbox/Inputbox.tsx
@@ -4,12 +4,17 @@ import S from './styles';
 
 export default function Inputbox({ placeholder, onChange, onBlur, value }) {
   return (
-    <S.InputBox
-      style={{ fontFamily: 'JockeyOne_400Regular' }}
-      placeholder={placeholder}
-      onChangeText={onChange}
-      onBlur={onBlur}
-      value={value}
-    />
+    <View>
+      <S.InputLabelBG colors={['#081E23', '#081E23', '#031013']}>
+        <S.InputLabel>{placeholder}</S.InputLabel>
+      </S.InputLabelBG>
+      <S.InputBox
+        style={{ fontFamily: 'JockeyOne_400Regular' }}
+        placeholder={''}
+        onChangeText={onChange}
+        onBlur={onBlur}
+        value={value}
+      />
+    </View>
   );
 }

9d4d3bf8a5e379685953449f16022821dd97d375
Christian Lam <christian.lam0103@gmail.com>
2024-08-24 10:22:17 +1000

    feat: Template for nodemailer

--- server/app.js
+++ server/app.js
@@ -1,11 +1,24 @@
 import functions from 'firebase-functions';
 import express from 'express';
+import nodemailer from 'nodemailer';
 import cors from 'cors';
 const app = express();
 const PORT = 5001;
 
-app.use(cors({ origin: true }));
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {},
+});
 
+contactEmail.verify((e) => {
+  if (e) {
+    console.log('Error', e);
+  } else {
+    console.log('ready to send');
+  }
+});
+app.use(express.json());
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('Test');
 });
@@ -14,7 +27,30 @@ app.listen(PORT, () => {
   console.log('Listening on Port:', PORT);
 });
 
-app.post('/user/verify_email', (req, res) => {});
+app.post('/user/verify_email', (req, res) => {
+  // Get data from form
+  console.log(req.params, req.body);
+
+  const mail = {
+    from: 'test',
+    to: '',
+    subject: 'verify code',
+    html: `
+      <p>Your verification code: 123</p>
+    `,
+  };
+
+  contactEmail.sendMail(mail, (e) => {
+    if (e) {
+      res.json(e);
+    } else {
+      res.status(200).json({
+        status: 'sent',
+        timestamp: Date.now(),
+      });
+    }
+  });
+});
 
 let api = functions.https.onRequest(app);
 

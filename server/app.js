04082253df4fdf5cd0de7904ed435cf8eed6dc68
Christian Lam <christian.lam0103@gmail.com>
2024-08-24 13:13:04 +1000

    feat: Add server and email template

--- server/app.js
+++ server/app.js
@@ -1,22 +1,57 @@
-import functions from 'firebase-functions';
 import express from 'express';
-import nodemailer from 'nodemailer';
+import { createTransport } from 'nodemailer';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import googleapis from 'googleapis';
+
 const app = express();
 const PORT = 5001;
 
-const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {},
-});
+dotenv.config({ path: '../.env' });
 
-contactEmail.verify((e) => {
-  if (e) {
-    console.log('Error', e);
-  } else {
-    console.log('ready to send');
+const { google } = googleapis;
+const OAUTH2 = google.auth.OAuth2;
+
+const createTransporter = async () => {
+  try {
+    const oauth2Client = new OAUTH2(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      'https://developers.google.com/oauthplayground'
+    );
+
+    oauth2Client.setCredentials({
+      refresh_token: process.env.REFRESH_TOKEN,
+    });
+
+    const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if (err) {
+          console.log('ERR', err);
+          reject();
+        }
+        resolve(token);
+      });
+    });
+
+    const transporter = createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.USER_EMAIL,
+        accessToken,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.clientSecret,
+        refreshToken: process.env.REFRESH_TOKEN,
+      },
+    });
+
+    return transporter;
+  } catch (err) {
+    return err;
   }
-});
+};
+
 app.use(express.json());
 app.use(cors());
 app.get('/', (req, res) => {
@@ -27,20 +62,21 @@ app.listen(PORT, () => {
   console.log('Listening on Port:', PORT);
 });
 
-app.post('/user/verify_email', (req, res) => {
+app.post('/user/verify_email', async (req, res) => {
   // Get data from form
   console.log(req.params, req.body);
 
   const mail = {
-    from: 'test',
-    to: '',
-    subject: 'verify code',
+    from: process.env.USER_EMAIL,
+    to: 'yamiremuru@gmail.com',
+    subject: 'Verify code',
     html: `
       <p>Your verification code: 123</p>
     `,
   };
 
-  contactEmail.sendMail(mail, (e) => {
+  let emailTransporter = await createTransporter();
+  await emailTransporter.sendMail(mail, (e) => {
     if (e) {
       res.json(e);
     } else {
@@ -50,8 +86,14 @@ app.post('/user/verify_email', (req, res) => {
       });
     }
   });
+  // contactEmail.sendMail(mail, (e) => {
+  //   if (e) {
+  //     res.json(e);
+  //   } else {
+  //     res.status(200).json({
+  //       status: 'sent',
+  //       timestamp: Date.now(),
+  //     });
+  //   }
+  // });
 });
-
-let api = functions.https.onRequest(app);
-
-export default api;

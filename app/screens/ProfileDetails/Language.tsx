a91ea56603296d1f9b1f13e90deef93291d189db
Christian Lam <christian.lam0103@gmail.com>
2024-09-03 11:12:22 +1000

    feat: Update dropdowns

--- app/screens/ProfileDetails/Language.tsx
+++ app/screens/ProfileDetails/Language.tsx
@@ -5,7 +5,6 @@ import Dropdown from '@components/Dropdown';
 import { formStore } from 'store';
 
 import languages from 'app/helper/languages.json';
-import { RNMultiSelect } from 'rn-multipicker';
 
 const Language = () => {
   const { control } = useForm({

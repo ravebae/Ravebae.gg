8e165a0b95472291d2540f6e355042f9939ea3be
marcusasdgg <marcusolachan@icloud.com>
2024-10-18 23:03:20 +1100

    feat: finished topbar of direct message screen

--- app/screens/Messages/DirectMessage.tsx
+++ app/screens/Messages/DirectMessage.tsx
@@ -3,14 +3,15 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { RouterProps } from '../List';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import styles from './style'
 /**
  * Function that servers as the screen for the direct message screen, obviously should take in some parameters, 
  * however no clue how we will internalise the message/user id system right now is the prototype ui stage.
  */
  function DirectMessageScreen(){    
     return (
-        <View>
-            <Text>Details</Text>
+        <View style={styles.backgroundView}>
+            <Text>Detailssss</Text>
         </View>
     )
 }
@@ -32,25 +33,34 @@ function DirectMessagePrototype(){
  */
 function messageTopBar({ navigation }: RouterProps){
     return {
-        title: "David",
+        title: "david",
         headerStyle: {
             backgroundColor: '#081e23', 
             borderBottomWidth: 1,
             borderBottomColor: '#ccc', 
-            elevation: 5,
+            elevation: 0,
+            height: 100, 
           },
-          headerTitleStyle: {
-            color: '#FFFFFF', 
-            fontWeight: 'bold', 
-          },
+        headerTitleStyle: {
+            color: 'white'
+        },
         headerLeft: () =>{
-            return <Ionicons name="chevron-back-circle-outline" onPress={() => navigation.goBack() } color= "#ea3ef7"/>
+            return <Ionicons name="chevron-back-circle" onPress={() => navigation.goBack() } color= "#ea3ef7" size={30} style={{ marginRight: 20 }}/>
         },
         headerRight: ()=>{
-            return <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Ionicons name="videocam" color= "#ea3ef7"/>
-                <Ionicons name="call" color= "#ea3ef7"/>
-                <Ionicons name="calendar" color= "#ea3ef7"/>
+            return <View style={{ flexDirection: 'row', alignItems: 'center', } }>
+                <View style={{ paddingHorizontal: 5 }}>
+                    <Ionicons name="call" color="#ea3ef7" size={30} />
+                </View>
+                <View style={{ paddingHorizontal: 5 }}>
+                    <Ionicons name="videocam" color="#ea3ef7" size={30} />
+                </View>
+                <View style={{ paddingHorizontal: 5 }}>
+                    <Ionicons name="calendar" color="#ea3ef7" size={30} />
+                </View>
+                <View style={{ paddingHorizontal: 5 }}>
+                    <Ionicons name="ellipsis-horizontal" color="#ea3ef7" size={30} />
+                </View>
             </View>
         }
     }
---
+++ app/screens/Messages/style.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    backgroundView: {
+        flex: 1, 
+        backgroundColor: '#081e23',
+    },
+});
+
+export default styles;

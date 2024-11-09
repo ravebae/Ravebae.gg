63d5990e15484dd1d4fdeb65233fc59c9df9e955
marcusasdgg <marcusolachan@icloud.com>
2024-11-09 18:11:42 +1100

    feat: added messagebar to screen

--- app/screens/Messages/DirectMessage.tsx
+++ app/screens/Messages/DirectMessage.tsx
@@ -1,20 +1,197 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { RouterProps } from '../List';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import styles from './style'
+import { GestureHandlerRootView, TextInput } from 'react-native-gesture-handler';
+import ChatBubble from 'react-native-chat-bubble';
+
+
+type itemData = {
+    message: string;
+    owner: boolean;
+    isImportant: boolean;
+    messageDate: string;
+    pictureUrl: string;
+}
+
+const data : itemData[] = [
+    {
+        message: "Hey Gurl!!!",
+        owner: false,
+        isImportant: false,
+        messageDate: "Thurs 25, Dec 9.19 PM",
+        pictureUrl: "",
+    },
+    {
+        message: "You Matched with David",
+        owner: true,
+        isImportant: true,
+        messageDate: "Thurs 25, Dec 9.19 PM",
+        pictureUrl: "",
+    },
+    {
+        message: "Wassup",
+        owner: true,
+        isImportant: false,
+        messageDate: "Thurs 25, Dec 9.19 PM",
+        pictureUrl: "",
+    },
+    {
+        message: "I'm Radiant",
+        owner: false,
+        isImportant: false,
+        messageDate: "Thurs 25, Dec 9.19 PM",
+        pictureUrl: "",
+    },
+    {
+        message: "Marry me!",
+        owner: true,
+        isImportant: false,
+        messageDate: "Thurs 25, Dec 9.19 PM",
+        pictureUrl: "",
+    },
+    {
+        message: "Marry me!",
+        owner: true,
+        isImportant: false,
+        messageDate: "Thurs 25, Dec 9.19 PM",
+        pictureUrl: "",
+    },
+    {
+        message: "Marry me!",
+        owner: true,
+        isImportant: false,
+        messageDate: "Thurs 25, Dec 9.19 PM",
+        pictureUrl: "",
+    },
+    {
+        message: "Marry me!",
+        owner: true,
+        isImportant: false,
+        messageDate: "Thurs 25, Dec 9.19 PM",
+        pictureUrl: "",
+    },
+    {
+        message: "Marry me!",
+        owner: true,
+        isImportant: false,
+        messageDate: "Thurs 25, Dec 9.19 PM",
+        pictureUrl: "",
+    },
+    {
+        message: "",
+        owner: true,
+        isImportant: false,
+        messageDate: "Thurs 25, Dec 9.19 PM",
+        pictureUrl: "https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/image8-2.jpg?width=595&height=400&name=image8-2.jpg",
+    },
+
+]
+
+
+type itemProps = {
+    item: itemData;
+    onPress: () => void;
+    ownerBackgroundColor: string, 
+    nonOwnerBackground: string,
+    textColor: string,
+}
+
+// Ui element pertaining to a message bubble and it's corresponding Column Element?
+const Item = ({item, onPress, ownerBackgroundColor, nonOwnerBackground, textColor}: itemProps) => {
+    if (item.pictureUrl === ""){
+        return (
+            <ChatBubble
+          isOwnMessage={item.owner}
+          bubbleColor={item.owner ? ownerBackgroundColor : nonOwnerBackground}
+          tailColor={item.owner ? ownerBackgroundColor : nonOwnerBackground}
+          withTail={false}
+          onPress={() => console.log("Bubble Pressed!")}
+        >
+          <Text style={{color: textColor}}>{item.message}</Text>
+        </ChatBubble>
+        )
+    } else {
+        if (item.owner){
+            console.log("returned owner")
+            return (
+                <Image
+                source={{
+                    uri: item.pictureUrl,
+                  }}
+                style={[styles.imageStyle,{}]}
+            />
+            )
+        } else {
+            console.log("returned nonwoern")
+            return (
+                <Image
+                source={{
+                    uri: item.pictureUrl,
+                  }}
+                style={styles.imageStyle}
+            />
+            )
+        }
+    }
+}
+
+
+    
+    
+
+const ImportantMiddleMessage = ({item}: itemProps) => (
+    <View style={styles.importantMessage}>
+        <Text style={[styles.importantText]}>{item.messageDate}</Text>
+        <Text style={styles.importantText}>{item.message}</Text>
+    </View>
+)
+
+
+const renderItem = ({item}: {item: itemData}) => {
+    if (item.isImportant){
+        return (
+            <ImportantMiddleMessage 
+                item={item}
+                ownerBackgroundColor='white'
+                textColor='White'
+                onPress={() => { } } nonOwnerBackground={''}            
+            />
+        )
+    } 
+    return (
+      <Item
+        item={item}
+        onPress={() => {}}
+        ownerBackgroundColor={"#e2bdff"}
+        nonOwnerBackground={"#ffffff"}
+        textColor={"Black"}
+      />
+    );
+  };
+
 /**
  * Function that servers as the screen for the direct message screen, obviously should take in some parameters, 
  * however no clue how we will internalise the message/user id system right now is the prototype ui stage.
  */
- function DirectMessageScreen(){    
+function DirectMessageScreen(){
+    const [sendText, setsendText] = useState('');
     return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
         <View style={styles.backgroundView}>
-            <Text>Detailssss</Text>
+            <FlatList
+                data={data}
+                renderItem={renderItem}
+                style={styles.messageScroll}
+            />
+            <MessageBottombar onsend={() => {}}/>
         </View>
+        </GestureHandlerRootView>
     )
 }
+
 
 /**
  * 
@@ -22,11 +199,32 @@ import styles from './style'
  */
 function DirectMessagePrototype(){    
     return (
-        <View>
+        <View style={styles.importantMessage}>
             
         </View>
     )
 }
+
+function MessageBottombar({onsend} : {onsend: () => void}){
+    const [sendText, setsendText] = useState('');
+    return (
+        <View style= {styles.messageBar}>
+            <TextInput
+                style={styles.searchBar}
+                placeholder="SEND MESSAGE"
+                value={sendText}
+                onChangeText={setsendText}
+                clearButtonMode="always"
+                placeholderTextColor={"#FFFF"}
+            />
+            <Ionicons name="image"  color= "#ea3ef7" size={33} style={[{alignSelf: "center", marginLeft: 20}]} />
+        </View>
+    )
+}
+
+
+
+
 
 /**
  * internal top bar function
@@ -37,7 +235,7 @@ function messageTopBar({ navigation }: RouterProps){
         headerStyle: {
             backgroundColor: '#081e23', 
             borderBottomWidth: 1,
-            borderBottomColor: '#ccc', 
+            borderBottomColor: '#081e23', 
             elevation: 0,
             height: 100, 
           },
@@ -65,5 +263,9 @@ function messageTopBar({ navigation }: RouterProps){
         }
     }
 }
+
+//timestamp every 5 hours
+
+
 
 export {DirectMessageScreen, DirectMessagePrototype, messageTopBar}
--- app/screens/Messages/style.ts
+++ app/screens/Messages/style.ts
@@ -7,6 +7,54 @@ const styles = StyleSheet.create({
         flex: 1, 
         backgroundColor: '#081e23',
     },
+    messageColumn : {
+        width: "100%",
+        justifyContent: "center",
+    },
+    importantMessage: {
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 60,
+    },
+    message : {
+        width: "100%",
+        justifyContent: "center",
+        height: 60,
+    },
+    importantText: {
+        color: "#000000",
+        fontSize: 12
+    },
+    messageScroll:{
+        height: "90%",
+        width: "100%",
+        paddingHorizontal: 20,
+    },
+    searchBar: {
+        height: "80%",
+        width: '80%',
+        backgroundColor: '#081e23',
+        borderRadius: 40,
+        paddingHorizontal: 30,
+        borderWidth: 1,
+        borderColor: '#ddd',
+      },
+      imageStyle: {
+        width: 100,
+        height: 100,
+        resizeMode: 'stretch',
+        borderRadius: 20
+      },
+    messageBar: {
+        height: "10%",
+        backgroundColor: '#081e23',
+        paddingLeft: 20,
+        flexDirection: "row",
+        justifyContent: 'flex-start', 
+        alignItems: 'center',  
+    }
+
 });
 
 export default styles;
